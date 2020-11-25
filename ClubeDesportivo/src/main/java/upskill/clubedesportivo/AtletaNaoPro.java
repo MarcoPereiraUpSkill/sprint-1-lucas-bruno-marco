239b5f924cc7ec5c1f07e5c1defd287fbf734e1b
Bruno Barbosa <brunoffcb@upskill.pt>
2020-11-25 14:23:07 +0000

    Javadoc Atualizado

--- ClubeDesportivo/src/main/java/upskill/clubedesportivo/AtletaNaoPro.java
+++ ClubeDesportivo/src/main/java/upskill/clubedesportivo/AtletaNaoPro.java
@@ -24,15 +24,15 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @param antiguidade
-     * @param nome
-     * @param nic
-     * @param genero
-     * @param idade
-     * @param fcr
-     * @param atividade
-     * @param objetivo
-     * @param premios
+     * @param antiguidade Número de anos que o atleta está afiliado ao clube.
+     * @param nome Nome do atleta.
+     * @param nic Número de identificação civil do atleta.
+     * @param genero Género do atleta.
+     * @param idade Idade do atleta.
+     * @param fcr Frequência cardíaca em repouso, valor que representa o número de batimentos cardíacos em repouso durante 1 minuto.
+     * @param atividade Atividade associada ao atleta: caminhada, corrida, ciclismo ou natação.
+     * @param objetivo Objetivo do atleta: queima de gordura ou capacidade cardiorrespiratória. Determina IT(Intensidade de Treino).
+     * @param premios Prémios arrecadados pelo atleta durante um mês.
      */
     public AtletaNaoPro(int antiguidade, String nome, int nic, String genero, int idade, int fcr, String atividade, String objetivo, double premios) {
         super(nome, nic, genero, idade, fcr, atividade, objetivo, premios);
@@ -49,7 +49,7 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @return
+     * @return Antiguidade
      */
     public int getAntiguidade() {
         return antiguidade;
@@ -57,7 +57,7 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @param antiguidade
+     * @param antiguidade Antiguidade
      */
     public void setAntiguidade(int antiguidade) {
         this.antiguidade = antiguidade;
@@ -87,7 +87,7 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @return
+     * @return Calcula percentagem de antiguidade mediante anos de antiguidade do atleta
      */
     public double obterPercAntiguidade() {
         return (antiguidade >= minAntiguidade && antiguidade <= medAntiguidade) ? percMinAntiguidade
@@ -96,7 +96,8 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @return
+     * @return Método chamado pelas subclasses AtletaSemiPro e AtletaAmador para calcular
+     * o valor mensal a pagar aos atletas SemiProfissionais e Amadores
      */
     @Override
     public double calcularValorMensal() {
@@ -105,7 +106,7 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @return
+     * @return Percentagem mediante o intervalo de antiguidade de [5,10] anos
      */
     public static double getPercMinAntiguidade() {
         return percMinAntiguidade;
@@ -113,7 +114,7 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @param percMinAntiguidade
+     * @param percMinAntiguidade Percentagem mediante o intervalo de antiguidade de [5,10] anos
      */
     public static void setPercMinAntiguidade(double percMinAntiguidade) {
         AtletaNaoPro.percMinAntiguidade = percMinAntiguidade;
@@ -121,7 +122,7 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @return
+     * @return Percentagem mediante o intervalo de antiguidade de ]10,20] anos
      */
     public static double getPercMedAntiguidade() {
         return percMedAntiguidade;
@@ -129,7 +130,7 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @param percMedAntiguidade
+     * @param percMedAntiguidade Percentagem mediante o intervalo de antiguidade de ]10,20] anos
      */
     public static void setPercMedAntiguidade(double percMedAntiguidade) {
         AtletaNaoPro.percMedAntiguidade = percMedAntiguidade;
@@ -137,7 +138,7 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @return
+     * @return Percentagem mediante antiguidades superiores a 20 anos
      */
     public static double getPercMaxAntiguidade() {
         return percMaxAntiguidade;
@@ -145,7 +146,7 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @param percMaxAntiguidade
+     * @param percMaxAntiguidade Percentagem mediante antiguidades superiores a 20 anos
      */
     public static void setPercMaxAntiguidade(double percMaxAntiguidade) {
         AtletaNaoPro.percMaxAntiguidade = percMaxAntiguidade;
@@ -153,7 +154,7 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @return
+     * @return Valor que determina o intervalo mínimo da primeira percentagem da Tabela 
      */
     public static int getMinAntiguidade() {
         return minAntiguidade;
@@ -161,7 +162,7 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @param minAntiguidade
+     * @param minAntiguidade Valor que determina o intervalo mínimo da primeira percentagem da Tabela 
      */
     public static void setMinAntiguidade(int minAntiguidade) {
         AtletaNaoPro.minAntiguidade = minAntiguidade;
@@ -169,7 +170,8 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @return
+     * @return Valor que determina o intervalo máximo da primeira percentagem da Tabela e
+     * o intervalo mínimo da segunda tabela
      */
     public static int getMedAntiguidade() {
         return medAntiguidade;
@@ -177,7 +179,8 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @param medAntiguidade
+     * @param medAntiguidade Valor que determina o intervalo máximo da primeira percentagem da Tabela e
+     * o intervalo mínimo da segunda tabela
      */
     public static void setMedAntiguidade(int medAntiguidade) {
         AtletaNaoPro.medAntiguidade = medAntiguidade;
@@ -185,7 +188,7 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @return
+     * @return Valor que determina o intervalo mínimo da terceira percentagem da Tabela 
      */
     public static int getMaxAntiguidade() {
         return maxAntiguidade;
@@ -193,7 +196,7 @@ public class AtletaNaoPro extends Atleta {
 
     /**
      *
-     * @param maxAntiguidade
+     * @param maxAntiguidade Valor que determina o intervalo mínimo da terceira percentagem da Tabela 
      */
     public static void setMaxAntiguidade(int maxAntiguidade) {
         AtletaNaoPro.maxAntiguidade = maxAntiguidade;
