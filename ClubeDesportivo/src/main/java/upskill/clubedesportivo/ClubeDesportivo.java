475013c1bf60f9f246b38b26562492ccebd46c8b
Bruno Barbosa <brunoffcb@upskill.pt>
2020-11-26 09:30:13 +0000

    Atualização Javadoc

--- ClubeDesportivo/src/main/java/upskill/clubedesportivo/ClubeDesportivo.java
+++ ClubeDesportivo/src/main/java/upskill/clubedesportivo/ClubeDesportivo.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 
+/**
+ *
+ * @author Vicious
+ */
 public class ClubeDesportivo {
 
     private String nome;
@@ -14,44 +18,81 @@ public class ClubeDesportivo {
     private static final Date DATA_POR_OMISSAO = new Date();
     private static final ArrayList<Atleta> ATLETAS_POR_OMISSAO = new ArrayList<Atleta>();
 
+    /**
+     *Construtor Completo
+     * @param nome Nome do clube desportivo
+     * @param data Data de fundação do clube
+     * @param atletas Contentor para armazentar os atletas
+     */
     public ClubeDesportivo(String nome, Date data, ArrayList<Atleta> atletas) {
         this.nome = nome;
         this.data = data;
         this.atletas = atletas;
     }
 
+    /**
+     *Construtor com variável nome como parâmetro
+     * @param nome Nome do clube desportivo
+     */
     public ClubeDesportivo(String nome) {
         this.nome = nome;
         data = DATA_POR_OMISSAO;
         atletas = ATLETAS_POR_OMISSAO;
     }
 
+    /**
+     *Construtor vazio
+     */
     public ClubeDesportivo() {
         nome = NOME_POR_OMISSAO;
         data = DATA_POR_OMISSAO;
         atletas = ATLETAS_POR_OMISSAO;
     }
 
+    /**
+     *Obter nome do clube
+     * @return Nome do clube
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     *Obter data de fundação
+     * @return Data de fundação
+     */
     public Date getData() {
         return data;
     }
 
+    /**
+     *Obter contentor de atletas
+     * @return Contentor de atletas
+     */
     public ArrayList<Atleta> getAtletas() {
         return atletas;
     }
 
+    /**
+     *Alterar nome do clube
+     * @param nome Nome do clube
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     *Alterar data de fundação
+     * @param data Data de fundação
+     */
     public void setData(Date data) {
         this.data = data;
     }
 
+    /**
+     *Alterar contentor de atletas
+     * @param atletas Contentor de atletas
+     */
     public void setAtletas(ArrayList<Atleta> atletas) {
         this.atletas = atletas;
     }
@@ -77,15 +118,27 @@ public class ClubeDesportivo {
                 && atletas.equals(clubeDesportivo.getAtletas());
     }
 
+    /**
+     *Insere atleta ao contentor de atletas do ClubeDesportivo
+     * @param atleta Contentor com dados do atleta
+     */
     public void inserirAtleta(Atleta atleta) {
         atletas.add(atleta);
     }
 
+    /**
+     *Ordena o contentor de atletas alfabeticamente por nome
+     * @return Contentor de atletas
+     */
     public ArrayList<Atleta> ordenarAlfabeticamenteNome() {
         Collections.sort(atletas);
         return atletas;
     }
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<Atleta> ordenarInversamentePremios() {
         OrdenarPremios ordenarPremios = new OrdenarPremios();
         Collections.sort(atletas, ordenarPremios.reversed());
@@ -93,6 +146,10 @@ public class ClubeDesportivo {
         return atletas;
     }
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<Atleta> ordenarCategoriaModalidadeNome() {
         OrdenarCategoriaModalidadeNome ordenarCategoriaModalidadeNome = new OrdenarCategoriaModalidadeNome();
         Collections.sort(atletas, ordenarCategoriaModalidadeNome);
@@ -100,6 +157,10 @@ public class ClubeDesportivo {
         return atletas;
     }
 
+    /**
+     *
+     * @return
+     */
     public double calcularTotalIRS() {
         double valorTotalIRS = 0;
         
